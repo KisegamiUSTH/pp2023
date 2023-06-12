6935ff721ea59a04637b36e233eb79b83f20cb22
KisegamiUSTH <anhnx.bi12-026@st.usth.edu.vn>
2023-06-12 22:25:28 +0700

    Combine Class with Main

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -1,5 +1,23 @@
-from students import Student
-from courses import Course
+class Course:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+
+
+class Student:
+    def __init__(self, id, name, dob):
+        self.id = id
+        self.name = name
+        self.dob = dob
+        self.courses = {}
+        self.marks = {}
+
+    def add_course(self, course_id, course_name):
+        self.courses[course_id] = course_name
+
+    def add_mark(self, course_id, mark):
+        self.marks[course_id] = mark
 
 students = {}
 courses = {}
